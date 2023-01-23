d487a00390baace42889aa7865d847329b8cda6d
808moon <moonesi.alireza@yahoo.com>
2023-01-24 00:45:55 +0330

    make js1 project

---
+++ js/main.js
@@ -0,0 +1,17 @@
+//document.body.innerHTML=`<h1>Hello World!</h1>`
+//document.write('hello world')
+//consol.log("Hello World!")
+/*const fetchPosts=async()=>{
+    let data=await fetch("https://jsonplaceholder.typicode.com/posts")
+    let response=await data.json()
+    console.log(response);
+}
+fetchPosts()*/
+//"  ' '  "
+//document.write("<h1 class='test'>Hello World!</h1>")
+//document.querySelector('div#root').innerHTML="<h1 class='test'>Hello World!</h1>"
+/*var x=100
+console.log(x);
+x=200
+let z=1000+x
+console.log(z);*/
