8ed4a7060515d60e38353c79e78eeff93b9b5dcd
808moon <moonesi.alireza@yahoo.com>
2023-01-24 13:11:31 +0330

    finish js1

--- js/main.js
+++ js/main.js
@@ -29,4 +29,111 @@ console.log(w);
 console.log(typeof w);
 var r=true
 console.log(r);
-console.log(typeof r);*/
+console.log(typeof r);
+
+var firstName="alireza"
+var lastName="moonesi"
+var email="alimoonesi@yahoo.com"
+var fatherName="ali"
+var age=25
+console.log(firstName)
+console.log(typeof firstName)
+console.log(lastName);
+console.log(typeof lastName);
+console.log(age)
+console.log(typeof age)
+console.log(fatherName);
+console.log(typeof fatherName);
+console.log(email);
+console.log(typeof email);
+
+var firstName="alireza"
+var lastName="moonesi"
+var email="alimoonesi@yahoo.com"
+var fatherName="ali"
+var age=25
+console.log("my name is "+firstName+" and my last name is "+lastName+" and my father name is "+fatherName+" and I'm "+age+" years old and my email is "+email);
+console.log("my name is\t"+firstName+" and my last name is\t"+lastName+" and my father name is\t"+fatherName+" and I'm "+age+" years old and my email is\t"+email);
+console.log("my name is\n"+firstName+" and my last name is\n"+lastName+" and my father name is\n"+fatherName+" and I'm "+age+" years old and my email is\n"+email);
+document.write("my name is"+"<br />"+firstName+" and my last name is"+"<br />"+lastName+" and my father name is"+"<br />"+fatherName+" and I'm "+age+" years old and my email is"+"<br />"+email);
+document.write(`<h1><span class='firstName'>my name is ${firstName}</span> and my lastname is ${lastName} and my father name is ${fatherName} and I'm ${age} years old and my email is ${email}</h1>`)
+console.log(`my name is ${firstName} and my lastname is ${lastName} and my father name is ${fatherName} and I'm ${age} years old and my email is ${email}`);
+
+var firstName="alireza"
+console.log(firstName);
+var firstName="karim"
+console.log(firstName);
+let firstName1="alireza"
+console.log(firstName1);
+let firstName2="karim"
+console.log(firstName2);
+const pi=3.14
+console.log(pi);
+pii=100
+console.log(pii);
+{
+    {
+        {
+            console.log(pii);
+        }
+    }
+}
+
+let x="alireza"
+console.log(x);
+console.log(typeof x);
+x=10
+console.log(x);
+console.log(typeof x);
+let z=100+x
+console.log(z);
+
+let firstName=prompt('enter your firstName :','alireza')
+console.log(firstName);
+console.log(typeof firstName);
+
+ let firstName=prompt("firstName :")
+ let lastName=prompt('lastName :')
+ let fatherName=prompt('father name :')
+ let age=prompt('age :')
+ let email=prompt('email :')
+ console.log(`my name is ${firstName} and my lastName is ${lastName} and my father name is ${fatherName} and I'm ${age} years old and my email is ${email}`);
+
+ let x= prompt("x :")
+ console.log(x);
+ console.log(typeof x);
+ let z=x+100
+ console.log(z);
+
+ let x= +prompt("x :")
+ console.log(x);
+ console.log(typeof x);
+ let z=x+100
+ console.log(z);
+
+ let x= parseInt(prompt("x :"))
+ console.log(x);
+ console.log(typeof x);
+ let z=x+100
+ console.log(z);
+
+ let x= parseFloat(prompt("x :"))
+ console.log(x);
+ console.log(typeof x);
+ let z=x+100
+ console.log(z);
+
+ let x= Number(prompt("x :"))
+ console.log(x);
+ console.log(typeof x);
+ let z=x+100
+ console.log(z.toString());
+ console.log(typeof z.toString());*/
+
+ let x= Number(prompt("x :"))
+ console.log(x);
+ console.log(typeof x);
+ let z=x+100
+ String(z)
+ console.log(z);
+ console.log(typeof z);
